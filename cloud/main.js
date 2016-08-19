d139d7c7284d0f641062f35370176ff2646433ce
developweb10 <developmentweb10@gmail.comgit config --global user.name DevelopAppsgit config --global user.email developmentweb10@gmail.com>
2016-08-19 13:39:04 +0530

    Updated main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,24 @@
+Parse.Cloud.define('sendPush', function(request, response) {
+ 
+    var pushQuery = new Parse.Query(Parse.Installation);
+    pushQuery.whereEqualTo("email", email);
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
-});
+
+   Parse.Push.send({
+   where: pushQuery,
+     data: {
+        alert: 'You have a new message from ' + request.params.username + '!'
+      }, 
+      push_time: request.params.date
+    }, {
+      success: function() {
+        // Push was successful
+        response.success('push successful')
+      },
+      error: function(error) {
+        // Handle error
+        response.error('push failed')
+      }
+   });
+
+    });
